ba416695d9b830753026d8cbfad52f0dc03ad032
Denis <den.batuev99@gmail.com>
2022-07-04 22:30:53 +0300

    initial commit

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите пятизначное число");
+string num = Console.ReadLine();
+int i = num.Length;
+
+if(i == 5)
+{
+    if(num[0] == num[i - 1] && num[1] == num[i - 2])
+    {
+        Console.WriteLine("Число является полиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Число не является полиндромом"); 
+    }
+}
+
---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задание 23
+
+int[] calculed(int num)
+{
+   int[] A = new int[num];
+   int i = 1;
+   while( i <= num)
+   {
+    A[i-1] = i * i * i;
+    Console.WriteLine(A[i-1]);
+    i++;}
+    return A; 
+
+}
+
+int[] result = calculed(3);
+
+
